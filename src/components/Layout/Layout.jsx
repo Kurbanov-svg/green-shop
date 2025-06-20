9bc5de018f16fd6a207af766e5e70a5a755d1de2
Kurbanov-svg <asinn975@gmail.com>
2025-06-20 19:20:31 +0600

    Добавил Layout.jsx

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,10 +2,6 @@ import React from "react";
 import Header from "./header/Header";
 import Footer from "./footer/Footer";
 import MainRoutes from "../../routes/MainRoutes";
-import Plannet from "../pages/Plannet/Plannet";
-import Sidebar from "./sidebar/Sidebar";
-import ListPlants from "../../home/ListPlants/ListPlants";
-import Home from "../pages/Home";
 
 const Layout = () => {
   return (
