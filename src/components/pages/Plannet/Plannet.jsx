bb20b77ef4373bd99016a383d3ccb89b8c5040ea
Kurbanov-svg <asinn975@gmail.com>
2025-06-20 19:41:11 +0600

    .

--- src/components/pages/Plannet/Plannet.jsx
+++ src/components/pages/Plannet/Plannet.jsx
@@ -7,21 +7,19 @@ const Plannet = () => {
   return (
     <div className="container">
       <section className={css.hero}>
-        <div className={css.heroText}>
-          <p className={css.welcome}>Welcome to Greenshop</p>
-          <h2>
-            LET'S MAKE A <br /> BETTER
-            <span>PLANET</span>
-          </h2>
+        <div className={css.heroContent}>
+          <h5 className={css.welcome}>Welcome to GreenShop</h5>
+          <h1>
+            Let's Make a <span className={css.grow}>Better Planet</span>
+          </h1>
           <p className={css.description}>
-            We are an online plant shop offering a wide range of cheap and
-            trendy plants. Use our plants to create an unique Urban Jungle.
-            Order your favorite plants!
+            Your go-to shop for trendy, affordable plants. Join the urban jungle
+            revolution with us.
           </p>
-          <button className={css.shopBtn}>SHOP NOW</button>
+          <button className={css.shopBtn}>Shop Now</button>
         </div>
-        <div className={css.heroimage}>
-          <img src={big} alt="" />
+        <div className={css.heroImage}>
+          <img src={big} alt="Plant" />
           <img src={small} alt="" />
         </div>
       </section>
